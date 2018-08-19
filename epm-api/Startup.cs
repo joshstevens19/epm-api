0505aa37cc9c77625e18b2556e4c257058449e15
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-19 22:34:13 +0100

    configured start up

--- epm-api/Startup.cs
+++ epm-api/Startup.cs
@@ -1,23 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Amazon.S3;
+using epm_api.Services;
 
 namespace epm_api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -26,6 +27,20 @@ namespace epm_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.Authority = "http://localhost:49936";
+                o.Audience = "apiApp";
+                o.RequireHttpsMetadata = false;
+            });
+
+            services.AddSingleton<IS3Service, S3Service>();
+            // services.AddDefaultAWSOptions(Configuration.GetAWSOptions()); - want to get it working from development.json
+            services.AddAWSService<IAmazonS3>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +55,13 @@ namespace epm_api
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            ////add this configuration for the middleware needed to validate the tokens
+            //app.UseAuthentication();
+
+            ////add this line to show the ERROR STATUS CODES
+            //app.UseStatusCodePages();
+
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }

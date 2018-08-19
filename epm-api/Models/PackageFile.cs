00b165c8b7ec11e0565cb5619d47bd04fb1a559c
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-19 22:32:15 +0100

    package file model

---
+++ epm-api/Models/PackageFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace epm_api.Models
+{
+    public class PackageFile
+    {
+        public Uri FileUrl { get; set; }
+        public string LocationInPackage { get; set; }
+
+        public PackageFile() { }
+
+        public PackageFile(Uri fileUrl, string locationInPackage)
+        {
+            this.FileUrl = fileUrl;
+            this.LocationInPackage = locationInPackage;
+        }
+    }
+}

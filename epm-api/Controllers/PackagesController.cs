cd16e55e3ed46b75af542d004b10b81a9d1a7c5f
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-19 22:31:40 +0100

    Creation of packages controller

---
+++ epm-api/Controllers/PackagesController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using epm_api.Models;
+using epm_api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace epm_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PackagesController : ControllerBase
+    {
+        private readonly IS3Service _client;
+
+        public PackagesController(IS3Service client)
+        {
+            this._client = client;
+        }
+
+        [HttpGet]
+        [Route(template: "{packageName}/latest")]
+        [Produces("application/json")]
+        public async Task<IActionResult> Get([FromRoute] string packageName)
+        {
+            string latestVersion = await this._client.GetLatestVersionOfPackge(packageName);
+
+            IReadOnlyCollection<PackageFile> packageFiles =
+                await this._client.GetPackageFilesDetails(packageName, latestVersion);
+
+            return this.Ok(packageFiles);
+        }
+    }
+}

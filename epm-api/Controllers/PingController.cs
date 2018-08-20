ad5bdd6efebf3731e093f8db2531d9f01753db93
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-20 21:02:35 +0100

    ping controller to let people know the service is alive

---
+++ epm-api/Controllers/PingController.cs
@@ -0,0 +1,25 @@
+﻿using epm_api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace epm_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        private readonly IS3Service _client;
+
+        public PingController(IS3Service client)
+        {
+            this._client = client;
+        }
+
+        [HttpGet]
+        [Route(template: "")]
+        public IActionResult Get()
+        {
+            // check s3 is alive maybe!? do not want to do pointless chargable API calls though..
+            return this.Ok();
+        }
+    }
+}

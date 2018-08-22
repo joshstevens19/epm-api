4ffe85e117e64e71865fd7df07151e5a6b820e87
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-22 23:43:47 +0100

    move IS3Service into service > interfaces folder

---
+++ epm-api/Services/Interfaces/IS3Service.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.S3;
+using epm_api.Models;
+
+namespace epm_api.Services.Interfaces
+{
+    public interface IS3Service
+    {
+        IAmazonS3 GetClient();
+        string GetBucketName();
+    }
+}

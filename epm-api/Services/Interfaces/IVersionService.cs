b56690501803487e1a7175b0341a2a15a3cd5a99
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-22 23:44:18 +0100

    created IVersionService to handle any version logic for epm

---
+++ epm-api/Services/Interfaces/IVersionService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using epm_api.Models;
+
+namespace epm_api.Services.Interfaces
+{
+    public interface IVersionService
+    {
+        Task<string> GetLatestVersionOfPackge(string packageName);
+    }
+}
---
+++ epm-api/Services/VersionService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+using epm_api.Services.Interfaces;
+
+namespace epm_api.Services
+{
+    public class VersionService : IVersionService
+    {
+        private readonly IS3Service _s3Service;
+        private readonly IAmazonS3 _client;
+
+        public VersionService(IS3Service s3Service)
+        {
+            this._s3Service = s3Service;
+            this._client = this._s3Service.GetClient();
+        }
+
+        /// <summary>
+        /// Gets the latest version of a package
+        /// </summary>
+        /// <param name="packageName">The package name</param>
+        /// <returns></returns>
+        public async Task<string> GetLatestVersionOfPackge(string packageName)
+        {
+            ListObjectsRequest request = new ListObjectsRequest
+            {
+                BucketName = this._s3Service.GetBucketName(),
+                Prefix = packageName + "/latestversion"
+            };
+
+            S3Object versionDetails = (await this._client.ListObjectsAsync(request)).S3Objects.FirstOrDefault();
+
+            return versionDetails?.Key.Split('@')[1];
+        }
+    }
+}

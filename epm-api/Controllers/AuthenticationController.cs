67dc94e34c0f5f1a36edf162e214a19b805f73fd
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-22 23:43:25 +0100

    created authentication controller

---
+++ epm-api/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using epm_api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace epm_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+    }
+}

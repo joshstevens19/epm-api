35aa50b62f9e0474b849be8132ac7dd9bd79496d
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-29 19:49:14 +0100

    AuthorizeApiController for methods which need authentication to execute

---
+++ epm-api/Controllers/AuthorizeApiController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace epm_api.Controllers
+{
+    [Authorize]
+    public class AuthorizeApiController : Controller
+    {
+
+    }
+}

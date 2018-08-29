849b2a5588d5555d77af1bf0cf5a1914f466d68d
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-29 19:51:24 +0100

    AuthenticationService

---
+++ epm-api/Services/AuthenticationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using epm_api.Services.Interfaces;
+
+namespace epm_api.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private IJwtService _jwtService;
+        public AuthenticationService(IJwtService jwtService)
+        {
+            this._jwtService = jwtService;
+        }
+
+        public async Task<Boolean> Login(string username, string password)
+        {
+            return await Task.FromResult(true);
+        }
+    }
+}
+
---
+++ epm-api/Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace epm_api.Services.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> Login(string username, string password);
+    }
+}

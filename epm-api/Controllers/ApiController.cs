873c81bfb2aeb469d6a9c6ad5a5e9f20241931b3
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-29 19:48:43 +0100

    added ApiController

---
+++ epm-api/Controllers/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace epm_api.Controllers
+{
+    public class ApiController : Controller
+    {
+
+    }
+}

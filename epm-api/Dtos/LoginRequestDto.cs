0a053a1e42b244bb75f9c35fd413a2b44af37c4b
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-30 19:05:31 +0100

    login request dto

---
+++ epm-api/Dtos/LoginRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace epm_api.Dtos
+{
+    public class LoginRequestDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public int? ExpiryMinutes { get; set; }
+    }
+}

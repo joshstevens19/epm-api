bc9a97d9c7acc9540d8130ff6c3e3bb0f0b2d0b5
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-02 12:29:06 +0100

    Created a enum for dynamo db tables - may change to DynamoDbContext later

---
+++ epm-api/Enums/DynamoDbTablesEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace epm_api.Enums
+{
+    public enum DynamoDbTablesEnum
+    {
+        [DisplayName("PackageDetails")]
+        PackageDetails = 0,
+        [DisplayName("Teams")]
+        Teams = 1,
+        [DisplayName("Users")]
+        Users = 2
+    }
+}

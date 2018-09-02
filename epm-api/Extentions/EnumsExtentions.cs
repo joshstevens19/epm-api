08ce82b40c045b0a477b92ae8244157d9a3f2648
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-02 12:29:16 +0100

    Wrote enums extentions

---
+++ epm-api/Extentions/EnumsExtentions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace epm_api.Extentions
+{
+    public static class EnumExtensions
+    {
+        public static string DisplayName(this Enum @enum)
+        {
+            string displayName = string.Empty;
+            FieldInfo[] fields = @enum.GetType().GetFields();
+            foreach (FieldInfo field in fields)
+            {
+                if (Attribute.GetCustomAttribute(field, typeof(DisplayNameAttribute)) is DisplayNameAttribute displayNameAttribute &&
+                    field.Name.Equals(@enum.ToString(), StringComparison.InvariantCultureIgnoreCase))
+                {
+                    displayName = displayNameAttribute.DisplayName;
+                    break;
+                }
+            }
+
+            return displayName;
+        }
+    }
+}

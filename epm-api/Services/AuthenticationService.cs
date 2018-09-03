89174e882d6374a52ae7288ac03c8d73cc8738d6
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-03 17:54:35 +0100

    logic for register in the authentication service

--- epm-api/Services/AuthenticationService.cs
+++ epm-api/Services/AuthenticationService.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model.Internal.MarshallTransformations;
+using epm_api.Dtos;
 using epm_api.Enums;
+using epm_api.Models;
 using epm_api.Services.Interfaces;
 
 namespace epm_api.Services
@@ -16,16 +20,45 @@ namespace epm_api.Services
             this._dynamoDbService = dynamoDbService;
         }
 
-        public async Task<bool> LoginAsync(string username, string password)
+        /// <summary>
+        /// Login the user in if the username and password are correct
+        /// </summary>
+        /// <param name="username">Users username</param>
+        /// <param name="password">Users password</param>
+        /// <returns></returns>
+        public async Task<UsersEntity> LoginAsync(string username, string password)
         {
             // may use context instead from dynamodb service makes more sense
             // will change later on when have time
-            Document doc = await this._dynamoDbService.GetItemAsync(DynamoDbTablesEnum.Users, username);
+            UsersEntity user = await this._dynamoDbService.GetItemAsync<UsersEntity>(username);
 
-            // no user found
-            if (doc == null) return false;
+            if (user == null || user.Password != password)
+            {
+                return null;
+            }
 
-            return doc["Password"] == password;
+            return user;
+        }
+
+        /// <summary>
+        /// This registers the user 
+        /// </summary>
+        /// <param name="userEntity">The user details</param>
+        /// <returns></returns>
+        public async Task<UsersEntity> RegisterAsync(UsersEntity userEntity)
+        {
+            try
+            {
+                // need to do some checks to make sure user does not already exists 
+                // use expressions
+                userEntity = await this._dynamoDbService.PutItemAsync<UsersEntity>(userEntity);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+            return userEntity;
         }
     }
 }

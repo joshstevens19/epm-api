b001848233458e05a53a026e01d6cdb3c65c4a52
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-03 17:53:31 +0100

    Register request dto

---
+++ epm-api/Dtos/RegisterRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace epm_api.Dtos
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Introduction { get; set; }
+    }
+}

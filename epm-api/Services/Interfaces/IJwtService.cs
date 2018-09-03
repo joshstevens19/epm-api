353b85cef72e20f7bbbf615f7fa91cadbe5c746f
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-03 17:55:40 +0100

    IJwtService

--- epm-api/Services/Interfaces/IJwtService.cs
+++ epm-api/Services/Interfaces/IJwtService.cs
@@ -1,8 +1,10 @@
-﻿namespace epm_api.Services.Interfaces
+﻿using epm_api.Models;
+
+namespace epm_api.Services.Interfaces
 {
     public interface IJwtService
     {
-        string GenerateToken(string username, int expiryMinutes = 30);
+        string GenerateToken(UsersEntity user, int expiryMinutes = 30);
         string UnPackJWT(string jwtToken);
     }
 }

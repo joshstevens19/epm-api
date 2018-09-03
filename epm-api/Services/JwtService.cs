e4b1b74edcdfb8f9fce8687c2710763467e52c01
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-03 17:54:18 +0100

    added some new claims to JWT token

--- epm-api/Services/JwtService.cs
+++ epm-api/Services/JwtService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using epm_api.Models;
 using epm_api.Services.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
@@ -15,13 +16,16 @@ namespace epm_api.Services
             this._configuration = configuration;
         }
 
-        public string GenerateToken(string username, int expiryMinutes = 30)
+        public string GenerateToken(UsersEntity user, int expiryMinutes = 30)
         {
             byte[] symmetricKey = Convert.FromBase64String(_configuration["SecurityKey"]);
 
             Claim[] claims = new[]
             {
-                new Claim(ClaimTypes.Name, username)
+                new Claim("emailAddress", user.EmailAddress),
+                new Claim("firstName", user.FirstName), 
+                new Claim("lastName", user.LastName), 
+                new Claim("introduction", user.Introduction)
             };
 
             SymmetricSecurityKey key = new SymmetricSecurityKey(symmetricKey);

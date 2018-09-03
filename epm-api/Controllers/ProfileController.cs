c89dfdef3ac3fcd56b58fc7ab17792e675b2769b
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-03 17:56:03 +0100

    empty profile control

---
+++ epm-api/Controllers/ProfileController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace epm_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+
+    }
+}

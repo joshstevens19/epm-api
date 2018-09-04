d9dc9e1f50fbb2c4e962dd2de08820cdc5f18455
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-04 17:45:14 +0100

    new package dtos

---
+++ epm-api/Dtos/PackageFilesRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace epm_api.Dtos
+{
+    public class PackageFilesRequestDto
+    {
+        [Required]
+        public string FileName { get; set; }
+
+        [Required]
+        public string FileContent { get; set; }
+    }
+}

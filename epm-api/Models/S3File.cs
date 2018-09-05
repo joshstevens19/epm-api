74d30cbe6dc982756a93f7c96000934092577e4f
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-05 18:14:04 +0100

    s3 file model

---
+++ epm-api/Models/S3File.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace epm_api.Models
+{
+    public class S3File
+    {
+        public S3File(string name, string content)
+        {
+            this.Name = name;
+            this.Content = content;
+        }
+
+        public string Name { get; set; }
+
+        public string Content { get; set; }
+    }
+}

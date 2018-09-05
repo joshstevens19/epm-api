ae5f12ab5a3384b618abbabf9d9fb59a63cb54d5
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-05 18:13:56 +0100

    basic EthereumPmMetaData - need to clarify how this is going to work

---
+++ epm-api/Models/EthereumPmMetaData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace epm_api.Models
+{
+    public class EthereumPmMetaData
+    {
+        public string GitHubLink { get; set; }
+        // NEED TO CONFIRM HOW I WANT TO DO THIS
+    }
+}

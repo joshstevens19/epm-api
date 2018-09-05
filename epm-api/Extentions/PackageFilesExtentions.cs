38abb6725f311f121c2280c9c5a88bfd5ca8b211
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-05 18:13:37 +0100

    Create a ToS3Files extention on PackageFile

---
+++ epm-api/Extentions/PackageFilesExtentions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using epm_api.Models;
+
+namespace epm_api.Extentions
+{
+    public static class PackageFilesExtentions
+    {
+        public static IReadOnlyCollection<S3File> ToS3Files(this IReadOnlyCollection<PackageFile> packageFiles)
+        {
+            IList<S3File> s3Files = new List<S3File>();
+            foreach (var file in packageFiles)
+            {
+                s3Files.Add(new S3File(file.FileName, file.FileContent));
+            }
+
+            return (IReadOnlyCollection<S3File>)s3Files;
+        }
+    }
+}

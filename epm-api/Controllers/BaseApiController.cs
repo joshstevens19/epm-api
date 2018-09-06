b69d75ae6b156a6a60ec2b7f3c8e4fe4105abb32
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-06 17:15:17 +0100

    GetJwt token extention method in base api controller, will move

--- epm-api/Controllers/BaseApiController.cs
+++ epm-api/Controllers/BaseApiController.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace epm_api.Controllers
 {
     public class BaseApiController : Controller
     {
-
+        public async Task<string> GetJwtToken()
+        {
+            return await HttpContext.GetTokenAsync("access_token");
+        }
     }
 }

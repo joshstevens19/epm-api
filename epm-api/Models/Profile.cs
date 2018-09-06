d085ce8dbead917c636a64aaa5dc2e500ab3526a
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-06 17:16:24 +0100

    Profile model created

---
+++ epm-api/Models/Profile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace epm_api.Models
+{
+    public class Profile
+    {
+        public Profile(string emailAddress, string firstName, string lastName, string introduction)
+        {
+            this.EmailAddress = emailAddress;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Introduction = introduction;
+        }
+
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Introduction { get; set; }
+    }
+}

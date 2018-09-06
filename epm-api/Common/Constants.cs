91aa62a407d863fea1f8e7b49be7a78d854802af
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-06 17:14:51 +0100

    Constant class added

---
+++ epm-api/Common/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace epm_api.Common
+{
+    public static class Constants
+    {
+        public const string EthereumPmJson = "ethereum-pm.json";
+    }
+}

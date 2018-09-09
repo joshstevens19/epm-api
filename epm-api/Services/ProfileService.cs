635536c3d33ab7c8f81ba72bb878ee51544d68f1
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-09 14:42:36 +0100

    Profile service to allow users to update information

---
+++ epm-api/Services/ProfileService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using epm_api.Models;
+using epm_api.Services.Interfaces;
+
+namespace epm_api.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly IDynamoDbService _dynamoDbService;
+        public ProfileService(IDynamoDbService dynamoDbService)
+        {
+            this._dynamoDbService = dynamoDbService;
+        }
+
+        public async Task<UsersEntity> UpdateDetails(UsersEntity updatedUserEntity)
+        {
+            UsersEntity currentUserEntity = await this._dynamoDbService.GetItemAsync<UsersEntity>(updatedUserEntity.Username);
+
+            if (currentUserEntity == null) return null;
+
+            currentUserEntity.FirstName = updatedUserEntity.FirstName ?? currentUserEntity.FirstName;
+            currentUserEntity.LastName = updatedUserEntity.LastName ?? currentUserEntity.LastName;
+            currentUserEntity.Introduction = updatedUserEntity.Introduction ?? currentUserEntity.Introduction;
+            currentUserEntity.UpdatedOn = DateTime.Now;
+
+            try
+            {
+                return await this._dynamoDbService.PutItemAsync<UsersEntity>(currentUserEntity);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}

082dbcb18f2ec231c6c1c27c2d1e5c163125d52c
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-09 14:38:49 +0100

    remove `GetJwtToken` method from base api controller as not used

--- epm-api/Controllers/BaseApiController.cs
+++ epm-api/Controllers/BaseApiController.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace epm_api.Controllers
 {
     public class BaseApiController : Controller
     {
-        public async Task<string> GetJwtToken()
-        {
-            return await HttpContext.GetTokenAsync("access_token");
-        }
     }
 }

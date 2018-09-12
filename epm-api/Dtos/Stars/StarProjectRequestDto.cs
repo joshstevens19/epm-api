f284db3dc91f6e15dd36a781ac1857c416581376
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-12 19:12:25 +0100

    Star dtos

---
+++ epm-api/Dtos/Stars/StarProjectRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace epm_api.Dtos.Stars
+{
+    public class StarProjectRequestDto
+    {
+        [Required]
+        public string PackageName { get; set; }
+    }
+}
---
+++ epm-api/Dtos/Stars/UnstarProjectRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace epm_api.Dtos.Stars
+{
+    public class UnstarProjectRequestDto
+    {
+        [Required]
+        public string PackageName { get; set; }
+    }
+}

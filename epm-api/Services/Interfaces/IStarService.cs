97c23b67245dd58c8f6970f509c236019948a851
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-12 19:12:34 +0100

    star service

---
+++ epm-api/Services/Interfaces/IStarService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using epm_api.Models;
+
+namespace epm_api.Services.Interfaces
+{
+    public interface IStarService
+    {
+        Task StarPackage(string packageName, string jwtUsername);
+        Task UnstarPackage(string packageName, string jwtUsername);
+        Task<IReadOnlyList<string>> GetStarredProjects(string jwtUsername);
+    }
+}
---
+++ epm-api/Services/StarService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+using epm_api.Entities;
+using epm_api.Models;
+using epm_api.Services.Interfaces;
+
+namespace epm_api.Services
+{
+    public class StarService : IStarService
+    {
+        private readonly IDynamoDbService _dynamoDbService;
+
+        public StarService(IDynamoDbService dynamoDbService)
+        {
+            this._dynamoDbService = dynamoDbService;
+        }
+
+        public async Task StarPackage(string packageName, string jwtUsername)
+        {
+            UsersEntity user = await this._dynamoDbService.GetItemAsync<UsersEntity>(jwtUsername);
+
+            if (user == null)
+                throw new Exception("Can not find your user in the db, please try again later");
+
+            if (user.Stars == null)
+                user.Stars = new List<string>();
+
+            if (!user.Stars.Contains(packageName))
+            {
+                user.Stars.Add(packageName);
+
+                await this._dynamoDbService.PutItemAsync<UsersEntity>(user);
+            }
+        }
+
+        public async Task UnstarPackage(string packageName, string jwtUsername)
+        {
+            UsersEntity user = await this._dynamoDbService.GetItemAsync<UsersEntity>(jwtUsername);
+
+            if (user == null)
+                throw new Exception("Can not find your user in the db, please try again later");
+
+            if (user.Stars != null)
+            {
+                if (user.Stars.Contains(packageName))
+                {
+                    user.Stars = user.Stars.Where(s => s != packageName).ToList();
+
+                    // db not updating at the moment for some reason!
+                    await this._dynamoDbService.PutItemAsync<UsersEntity>(user);
+                }
+            }
+        }
+
+        public async Task<IReadOnlyList<string>> GetStarredProjects(string jwtUsername)
+        {
+            UsersEntity user = await this._dynamoDbService.GetItemAsync<UsersEntity>(jwtUsername);
+
+            if (user == null)
+                throw new Exception("Can not find your user in the db, please try again later");
+
+            return user.Stars;
+        }
+    }
+}

759e99a0e9ef2649bd096a80995aa164c35f64e7
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-17 18:15:50 +0100

    RefreshTokenDto

---
+++ epm-api/Dtos/Tokens/RefreshTokenDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace epm_api.Dtos.Tokens
+{
+    public class RefreshTokenDto
+    {
+        [Required]
+        public string CurrentJwtToken { get; set; }
+    }
+}

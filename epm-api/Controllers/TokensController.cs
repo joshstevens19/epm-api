7d0c15c841f3e34ef20cef1c405c68f41345faad
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-17 18:14:42 +0100

    create token controller

---
+++ epm-api/Controllers/TokensController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using epm_api.Dtos;
+using epm_api.Dtos.Authentication;
+using epm_api.Dtos.Extentions;
+using epm_api.Dtos.Teams;
+using epm_api.Models;
+using epm_api.Services;
+using epm_api.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace epm_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokensController : AuthorizeApiController
+    {
+        private readonly IJwtService _jwtService;
+
+        public TokensController(IJwtService jwtService)
+        {
+            this._jwtService = jwtService;
+        }
+
+        /// <summary>
+        /// Validates if a jwt is still valid 
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        [HttpGet]
+        [Route(template: "validate")]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public IActionResult Get()
+        {
+            return this.Ok();
+        }
+
+        /// <summary>
+        /// Validates if a jwt is still valid 
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        [HttpGet]
+        [Route(template: "refresh")]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public IActionResult Post([FromBody] RefreshTokenDto refreshTokenDto)
+        {
+            int expiryMinutes = 30;
+            DateTime expiryDate = DateTime.Now.AddMinutes(expiryMinutes);
+
+            string jwtToken = this._jwtService.RefreshToken(User.Claims.ToArray(), expiryMinutes);
+
+            return this.Ok(new JwtDetails(jwtToken, expiryDate));
+        }
+
+    }
+}

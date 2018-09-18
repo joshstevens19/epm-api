be679334db88412157bcf1b998951a065a5a2338
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-18 08:12:16 +0100

    Rename packages folder name due to git ignore to something which can be checked in

---
+++ epm-api/Dtos/EpmPackages/UnpublishPackageRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace epm_api.Dtos.EpmPackages
+{
+    public class UnpublishPackageRequestDtos
+    {
+        [Required]
+        public string PackageName { get; set; }
+    }
+}
---
+++ epm-api/Dtos/EpmPackages/UploadPackageRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using epm_api.Dtos.EpmPackages;
+
+namespace epm_api.Packages.Dtos.EpmPackages
+{
+    public class UploadPackageRequestDto
+    {
+        [Required]
+        public string PackageName { get; set; }
+
+        [Required]
+        public string PackageVersion { get; set; }
+
+        [Required]
+        public IReadOnlyCollection<PackageFilesRequestDto> PackageFiles { get; set; }
+    }
+}

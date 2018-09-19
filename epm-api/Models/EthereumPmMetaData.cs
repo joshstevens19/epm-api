c65258edd4a28ad5e49ec5afe9acae173992a89e
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-19 08:12:41 +0100

    add description and keyword in ethereum pm meta data

--- epm-api/Models/EthereumPmMetaData.cs
+++ epm-api/Models/EthereumPmMetaData.cs
@@ -7,8 +7,10 @@ namespace epm_api.Models
 {
     public class EthereumPmMetaData
     {
+        public string Description { get; set; }
         public string GitHub { get; set; }
         public bool Private { get; set; }
         public string Team { get; set; }
+        public IList<string> Keywords { get; set; }
     }
 }

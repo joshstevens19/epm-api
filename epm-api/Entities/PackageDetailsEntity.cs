17dbafec1fbee90a5968e62a447e7a82dd86a20a
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-19 08:12:23 +0100

    add new properties into package details entity

--- epm-api/Entities/PackageDetailsEntity.cs
+++ epm-api/Entities/PackageDetailsEntity.cs
@@ -11,14 +11,16 @@ namespace epm_api.Entities
     {
         [DynamoDBHashKey]
         public string PackageName { get; set; }
-        public List<string> Version { get; set; }
+        public IList<string> Version { get; set; }
+        public string Description { get; set; }
         public bool Private { get; set; }
         public string Team { get; set; }
         public string Owner { get; set; }
         public string LatestVersion { get; set; }
         public string GitHub { get; set; }
         public bool Deprecated { get; set; }
-        public List<string> AdminUsers { get; set; }
+        public IList<string> AdminUsers { get; set; }
+        public IList<string> Keywords { get; set; }
         public DateTime CreatedOn { get; set; }
     }
 }

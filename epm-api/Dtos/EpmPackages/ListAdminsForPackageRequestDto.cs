c38ef6f23eed1608568b1d15296c03b3936c829a
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-20 17:23:47 +0100

    new dtos

---
+++ epm-api/Dtos/EpmPackages/ListAdminsForPackageRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace epm_api.Packages.Dtos.EpmPackages
+{
+    public class ListAdminsForPackageRequestDto
+    {
+        [Required]
+        public string PackageName { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+    }
+}
